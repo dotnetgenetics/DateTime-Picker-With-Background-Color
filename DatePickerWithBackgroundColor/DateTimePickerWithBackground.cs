60c492e9614730f2f9cc61e2c689ac18c6a3f3eb
dotnetgenetics <gregory_keyboard[@]outlook.com>
2017-07-06 15:19:52 +0800

    Initial check-in of custom DateTimePicker control

---
+++ DatePickerWithBackgroundColor/DateTimePickerWithBackground.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatePickerWithBackgroundColor
+{
+    public class DateTimePickerWithBackground : DateTimePicker
+    {
+        private Bitmap _bmp;
+
+        enum BorderSize
+        {
+            One = 1,
+            Two = 2
+        };
+
+        public DateTimePickerWithBackground()
+        {
+            _bmp = new Bitmap(ClientRectangle.Height, ClientRectangle.Width);
+            this.SetStyle(ControlStyles.UserPaint, true);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == 0xf) //WM_PAINT message
+            {   
+                Graphics g = Graphics.FromHwnd(m.HWnd);
+                g.DrawImage(_bmp, ClientRectangle.Width - Convert.ToInt32(9 * ClientRectangle.Width / 100), 2);
+                g.Dispose();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.FillRectangle(new SolidBrush(Color.LawnGreen), ClientRectangle);
+            ControlPaint.DrawBorder(e.Graphics, ClientRectangle,
+                                   Color.LightGray, (int)BorderSize.One, ButtonBorderStyle.Solid,
+                                   Color.LightGray, (int)BorderSize.One, ButtonBorderStyle.Solid,
+                                   Color.LightGray, (int)BorderSize.One, ButtonBorderStyle.Solid,
+                                   Color.LightGray, (int)BorderSize.One, ButtonBorderStyle.Solid);
+            TextRenderer.DrawText(e.Graphics, Text, Font, Rectangle.FromLTRB(0, 0, Width - Height, Height), 
+                SystemColors.ControlText);
+            Image img = Properties.Resources.calendar_picker;
+            _bmp = new Bitmap(img, new Size(img.Width, img.Height));
+        }
+    }
+}
---
+++ DatePickerWithBackgroundColor/FDateTimePicker.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace DatePickerWithBackgroundColor
+{
+    partial class FDateTimePicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePickerWithBackground1 = new DatePickerWithBackgroundColor.DateTimePickerWithBackground();
+            this.SuspendLayout();
+            // 
+            // dateTimePickerWithBackground1
+            // 
+            this.dateTimePickerWithBackground1.Location = new System.Drawing.Point(33, 58);
+            this.dateTimePickerWithBackground1.Name = "dateTimePickerWithBackground1";
+            this.dateTimePickerWithBackground1.Size = new System.Drawing.Size(218, 20);
+            this.dateTimePickerWithBackground1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.dateTimePickerWithBackground1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DateTimePickerWithBackground dateTimePickerWithBackground1;
+    }
+}
+
